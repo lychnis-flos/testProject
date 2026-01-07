91dc62d9d643d159b8dc8fc8ce436972097a6204
lychnis-flos <2025m835@hokuriku-u.ac.jp>
2026-01-07 16:30:11 +0900

    fix: allow current

--- devProject/settings.py
+++ devProject/settings.py
@@ -137,4 +137,3 @@ LOGIN_REDIRECT_URL = '/'  # 假设 '/' 是您的 timeline 页面
 # 或者使用您在 urls.py 中定义的 timeline 名称
 # LOGIN_REDIRECT_URL = reverse_lazy('timeline') # 注意：如果使用 reverse_lazy 需要在 settings.py 顶部导入
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
